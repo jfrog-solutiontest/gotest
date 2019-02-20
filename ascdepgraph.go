ee38e2cb06269eeb31c9090bdc015ecad571cfd5
jfrogsolutionci <jfrog-solutions-ci@jfrog.com>
2019-02-20 14:44:27 -0800

    ascending dep graph test

---
+++ ascdepgraph.go
@@ -0,0 +1,11 @@
+package gotest
+
+import (
+  "fmt"
+  "github.com/kr/pty"
+  "github.com/jfrog-solutiontest/employee"
+)
+
+func MissingGoPackageName () {
+   fmt.Println("Package Name: Missing Go Package Test")
+}
---
+++ go.mod
@@ -0,0 +1,6 @@
+module github.com/jfrog-solutiontest/gotest
+
+require (
+	github.com/jfrog-solutiontest/employee v1.1.0
+	github.com/kr/pty v1.1.3
+)
